50c9e0f822e68381777c663713a590b6cb9d7ef1
DistantDig <jameswaller711@gmail.com>
2023-10-23 11:34:36 -0700

    added placeholder file generation

---
+++ utils/generateSVG.js
@@ -0,0 +1,21 @@
+// <-- SVG GENERATION -->
+
+// Function to generate svg file for logo
+function generateSVG(data) {
+    const svg = 
+    `<svg version="1.1"
+    width="300" height="200"
+    xmlns="http://www.w3.org/2000/svg">
+
+ <rect width="100%" height="100%" fill="red" />
+
+ <circle cx="150" cy="100" r="80" fill="green" />
+
+ <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">SVG</text>
+
+</svg>`;
+    return svg;
+  }
+  
+  
+  module.exports = {generateSVG};
