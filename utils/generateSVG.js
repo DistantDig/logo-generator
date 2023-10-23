24710611919c28b9ea5074c5f2f33957e1f86259
DistantDig <jameswaller711@gmail.com>
2023-10-23 13:00:34 -0700

    created renderText function to implement character limit

--- utils/generateSVG.js
+++ utils/generateSVG.js
@@ -1,5 +1,13 @@
 const generateShapes = require('./generateShapes');
 
+function renderText(text, textColor) {
+    if (text.length > 3) {
+        const cutLength = 3 - text.length;
+        text = text.slice(0, cutLength);
+    }
+    return `<text x="145" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>`
+}
+
 // Function to generate svg file for logo
 function generateSVG(data) {
     const svg = 
@@ -7,13 +15,12 @@ function generateSVG(data) {
     width="300" height="200"
     xmlns="http://www.w3.org/2000/svg">
 
- ${generateShapes.generateShapes(data.shape, data.backgroundColor)}
+ ${generateShapes.renderShapes(data.shape, data.backgroundColor)}
 
- <text x="145" y="125" font-size="60" text-anchor="middle" fill="${data.textColor}">${data.text}</text>
+ ${renderText(data.text, data.textColor)}
 
 </svg>`;
     return svg;
 }
   
-  
-  module.exports = {generateSVG};
+module.exports = {generateSVG};
